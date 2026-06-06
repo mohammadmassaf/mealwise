a1e84700f9b61050c7c6351ebe78c00731fb8e60
mohammadmassaf <mohammadmassaf1@gmail.com>
2026-06-06 23:49:36 +0300

    feat  : created DB MODELS

--- backend/app/models/db_models.py
+++ backend/app/models/db_models.py
@@ -1,22 +1,39 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey , Date , Float
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()   
 
-class User(Base):
+class Users(Base):
     __tablename__ = "users"
     user_id = Column(Integer , primary_key = True)
     username = Column(String, nullable=False)
     email = Column(String, nullable=False)
     password = Column(String,nullable = False)
-class User(Base):
-    __tablename__ = "users"
+class MealPlan(Base):
+    __tablename__ = "meal_plan"
+    plan_id = Column(Integer , primary_key = True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    num_days = Column(Integer, nullable = False)
+    start_date = Column(Date , nullable = False)
 
-class User(Base):
-    __tablename__ = "users"
 
-class User(Base):
-    __tablename__ = "users"
+class DayPlan(Base):
+    __tablename__ = "day_plan"
+    day_id = Column(Integer , primary_key = True)
+    plan_id = Column(Integer, ForeignKey("meal_plan.plan_id"))
+    day = Column(String , nullable = False)
 
-class User(Base):
-    __tablename__ = "users"
+
+class Meal(Base):
+    __tablename__ = "meal"
+    meal_id = Column(Integer , primary_key = True)
+    day_id = Column(Integer, ForeignKey("day_plan.day_id"))
+    name = Column(String, nullable = False)
+    calories = Column(Float , nullable = False)
+    time_to_cook = Column(Float     )
+
+class Ingredients(Base):
+    __tablename__ = "ingredients"
+    ing_id = Column(Integer , primary_key = True)
+    meal_id = Column(Integer, ForeignKey("meal.meal_id"))
+    name = Column(String , nullable = False)
